db549ecd8a024d9bb63c310fd31421c80e75e9c9
Rodrigo Monteiro <falecom@rodrigomonteiro.net>
2014-10-13 09:07:59 -0300

    Update zimbraBackup.sh

--- zimbraBackup.sh
+++ zimbraBackup.sh
@@ -8,6 +8,11 @@
 #                                                                             #
 # Instructions:                                                               #
 #                                                                             #
+# Script to backup Zimbra (MySQL + OpenLDAP + Mail + Contacts + Calendar +    #
+#   Tasks + Lists).                                                           #
+# The MySQL and OpenLDAP is for disaster/recover (full server) while the rest #
+#   individual.                                                               #
+#                                                                             #
 # The default directory for the scripts is /opt/scripts/zimbraBackup          #
 #   mkdir -p /opt/scripts/zimbraBackup                                        #
 #                                                                             #
